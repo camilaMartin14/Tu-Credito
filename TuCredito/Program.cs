fa7f9221a68365f55e1791e86f4c011eefdbd6e9
CamilaMartin14 <camilamartin1403@gmail.com>
2025-12-27 23:43:55 -0300

    definir configuraciones iniciales en Backend

--- TuCredito/Program.cs
+++ TuCredito/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TuCredito.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<TuCreditoContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CamilaConnection")));
+    //options.UseSqlServer(builder.Configuration.GetConnectionString("AylenConnection")));
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +23,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
 
 app.UseHttpsRedirection();
 
