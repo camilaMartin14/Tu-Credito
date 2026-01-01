12ffb33a594beeeb6d8920e3faed94a321aa8958
CamilaMartin14 <camilamartin1403@gmail.com>
2026-01-01 18:55:27 -0300

    definir dtos prestamista

---
+++ TuCredito-API/DTOs/PrestamistaLoginDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace TuCredito.DTOs
+{
+    public class PrestamistaLoginDTO
+    {
+        public string Usuario { get; set; } = string.Empty;
+        public string Contraseña { get; set; } = string.Empty;  
+    }
+}
--- TuCredito-API/Services/Implementations/PrestamistaService.cs
+++ TuCredito-API/Services/Implementations/PrestamistaService.cs
@@ -1,6 +1,36 @@
-﻿namespace TuCredito.Services.Implementations
+﻿using TuCredito.DTOs;
+using TuCredito.Models;
+using TuCredito.Repositories.Interfaces;
+using TuCredito.Services.Interfaces;
+
+namespace TuCredito.Services.Implementations
 {
-    public class PrestamistaService
+    public class PrestamistaService : IPrestamistaService
     {
+        private readonly IPrestamistaRepository _repository;
+        public PrestamistaService(IPrestamistaRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<Prestamista?> LoginAsync(string email, string contrasenia)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Prestamista?> ObtenerPrestamistaPorEmailAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Prestamista?> ObtenerPrestamistaPorIdAsync(int idPrestamista)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> RegistrarPrestamistaAsync(PrestamistaRegisterDto dto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
