ceae88f4e42db07488951c4a9399094d53c5f982
CamilaMartin14 <camilamartin1403@gmail.com>
2026-01-01 18:46:00 -0300

    terminar repo prestamista

--- TuCredito-API/Repositories/Implementations/PrestamistaRepository.cs
+++ TuCredito-API/Repositories/Implementations/PrestamistaRepository.cs
@@ -1,6 +1,33 @@
-﻿namespace TuCredito.Repositories.Implementations
+﻿using Microsoft.EntityFrameworkCore;
+using TuCredito.Models;
+using TuCredito.Repositories.Interfaces;
+
+namespace TuCredito.Repositories.Implementations
 {
-    public class PrestamistaRepository
+    public class PrestamistaRepository : IPrestamistaRepository
     {
+        private readonly TuCreditoContext _context;
+        public PrestamistaRepository(TuCreditoContext context)
+        {
+            _context = context;
+        }
+        public async Task<Prestamista?> ObtenerPrestamistaPorEmail(string email)
+        {
+            return await _context.Prestamista
+                .FirstOrDefaultAsync(p => p.Correo == email);
+        }
+
+        public async Task<Prestamista?> ObtenerPrestamistaPorId(int idPrestamista)
+        {
+            return await _context.Prestamista
+                .FirstOrDefaultAsync(p => p.Id == idPrestamista);
+        }
+
+        public async Task<int> RegistrarPrestamista(Prestamista p)
+        {
+            _context.Prestamista.Add(p);
+            await _context.SaveChangesAsync();
+            return p.Id;
+        }
     }
 }
